e40a833d848496c32832b4a7b08a230e1d4c77f1
Mbungai-Francesco <mbungaifrancesco2@gmail.com>
2026-05-02 03:43:47 +0200

    setcookies modified

--- controller/AuthController.ts
+++ controller/AuthController.ts
@@ -1,6 +1,8 @@
 import { db } from "../lib/db";
 import { Request, Response } from "express";
 import { generateToken } from "../utils/jwt";
+import { setTokenCookie } from "../utils/cookies";
+
 
 export const createUser = async (req: Request, res: Response) => {
 	try {
@@ -33,12 +35,7 @@ export const createUser = async (req: Request, res: Response) => {
 
 		const token = generateToken(user.id);
 
-		res.cookie("token", token, {
-			httpOnly: true,
-			secure: process.env.NODE_ENV === "production",
-			sameSite: "strict",
-			maxAge: 7 * 24 * 60 * 60 * 1000,
-		});
+		setTokenCookie(res, token);
 
 		return res.status(201).json({ message: "User created", data: user });
 	} catch (error: any) {
@@ -80,12 +77,7 @@ export const loginUser = async (req: Request, res: Response) => {
 
 		const token = generateToken(user.id);
 
-		res.cookie("token", token, {
-			httpOnly: true,
-			secure: process.env.NODE_ENV === "production",
-			sameSite: "strict",
-			maxAge: 7 * 24 * 60 * 60 * 1000,
-		});
+		setTokenCookie(res, token);
 
 		const { password: _, ...userWithoutPassword } = user;
 
---
+++ utils/cookies.ts
@@ -0,0 +1,12 @@
+// utils/cookie.ts
+import { Response } from "express";
+
+export const setTokenCookie = (res: Response, token: string) => {
+	const isProduction = process.env.NODE_ENV === "production";
+	res.cookie("token", token, {
+		httpOnly: true,
+		secure: isProduction,
+		sameSite: isProduction ? "none" : "lax",
+		maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+	});
+};
